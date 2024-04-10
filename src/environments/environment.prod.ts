37a1c6b7265b2980415efbca6d0285569ba70d23
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-10 11:51:45 +0530

    Add environment variable file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    BASE_URL: "https://link-union-backend.onrender.com",
+    // Other production environment variables if you have
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    BASE_URL: "https://link-union-backend.onrender.com",
+    // Other production environment variables if you have
+  };
